cd8c0d1ad3225e9964caa275794b1d70ef02e218
Angharad Stell <as16992@bristol.ac.uk>
2021-06-08 12:37:10 +0100

    add first attempt at making models

---
+++ n2o_inv/intermediates/make_models.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# read in variables
+cd ../spinup
+source bash_var.sh
+
+# all threee scripts need this
+export INVERSION_BASE_PARTIAL=${paths[wombat_paper]}/3_inversion/src/partials/base.R
+
+# choose model case
+case=IS-FIXEDAO-FIXEDWO5-NOBIAS
+
+# Make process model
+Rscript ${paths[wombat_paper]}/3_inversion/src/process-model.R \
+--control-emissions ${paths[geos_inte]}/control-emissions.fst \
+--perturbations ${paths[geos_inte]}/perturbations.fst \
+--control-mole-fraction ${paths[geos_inte]}/control-mole-fraction.fst \
+--sensitivities ${paths[geos_inte]}/sensitivities.fst \
+--output ${paths[geos_inte]}/process-model.rds
+
+# Make measurement model
+Rscript ${paths[wombat_paper]}/3_inversion/src/measurement-model.R \
+--observations ${paths[geos_inte]}/observations.fst \
+--process-model ${paths[geos_inte]}/process-model.rds \
+--output ${paths[geos_inte]}/measurement-model.rds
+
+# Make real model case
+Rscript ${paths[location_of_this_file]}/../intermediates/real-model-case.R \
+--case $case \
+--measurement-model ${paths[geos_inte]}/measurement-model.rds \
+--process-model ${paths[geos_inte]}/process-model.rds \
+--output ${paths[geos_inte]}/real-model-$case.rds
---
+++ n2o_inv/intermediates/real-model-case.R
@@ -0,0 +1,76 @@
+source(Sys.getenv('INVERSION_BASE_PARTIAL'))
+library(Matrix)
+library(WoodburyMatrix)
+
+set.seed(20200706)
+
+as_case <- function(process_model, measurement_model) {
+  list(
+    process_model = process_model,
+    measurement_model = measurement_model
+  )
+}
+
+args <- arg_parser('', hide.opts = TRUE) %>%
+  add_argument('--case', '') %>%
+  add_argument('--measurement-model', '') %>%
+  add_argument('--process-model', '') %>%
+  add_argument('--output', '') %>%
+  parse_args()
+
+log_info_case <- function(str, ...) {
+  log_info(paste0(
+    sprintf('[%s] ', args$case),
+    str
+  ), ...)
+}
+
+log_info_case('Loading measurement model')
+measurement_model <- readRDS(args$measurement_model)
+
+log_info_case('Loading process model')
+process_model <- readRDS(args$process_model)
+
+log_info_case('Constructing case')
+case_parts <- as.vector(stringr::str_split(
+  args$case,
+  '-',
+  simplify = TRUE
+))
+
+measurement_model <- measurement_model %>%
+  filter(overall_observation_mode %in% case_parts)
+
+
+n_groups <- nlevels(measurement_model$attenuation_factor)
+
+if ('FIXEDAO' %in% case_parts) {
+  process_model[['a']] <- c(rep(NA, 12), rep(0, 11))
+}
+
+if ('FIXEDWO5' %in% case_parts) {
+  process_model[['w']] <- c(rep(NA, 12), rep(4, 11))
+}
+
+process_model$eta_prior_mean <- rep(0, ncol(process_model$Psi))
+process_model$eta_prior_precision <- Diagonal(x = 1 / 25, n = ncol(process_model$Psi))
+
+if ('NOBIAS' %in% case_parts) {
+  measurement_model$A <- matrix(0, nrow = nrow(measurement_model$A), ncol = 0)
+  measurement_model$beta_prior_mean <- rep(0, 0)
+  measurement_model$beta_prior_precision <- Matrix::Diagonal(n = 0)
+}
+
+# HACK(mgnb): later code is configured to load these if missing
+process_model$sensitivities <- NULL
+process_model$H <- NULL
+
+output <- as_case(
+  process_model,
+  measurement_model
+)
+
+log_info_case('Saving')
+saveRDS_gz1(output, args$output)
+
+log_info_case('Done')
