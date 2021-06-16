64cfdccd838ca79ac4676c6b4559a0beddf8e650
Angharad Stell <as16992@bristol.ac.uk>
2021-06-16 11:47:25 +0100

    make into submitable batch job, stop sensitivity for obs before perturbation

--- n2o_inv/intermediates/process_geos_output.py
+++ n2o_inv/intermediates/process_geos_output.py
@@ -1,8 +1,7 @@
 import configparser
-import datetime
-from n2o_inv.obs.format_obspack_geoschem import PERTURB_END
 from pathlib import Path
 import re
+import sys
 
 import numpy as np
 import pandas as pd
@@ -16,7 +15,6 @@ def monthly_mean_obspack_id(site, date):
     identifier = f"{site}{date.strftime('%Y%m')}"
     obspack_id = f"obspack_multi-species_1_CCGGSurfaceFlask_v2.0_2021-02-09~n2o_{site}_surface-flask_1_ccgg_Event~{identifier}"
     return obspack_id
-
 
 def obspack_geos_preprocess(ds):
     """
@@ -61,80 +59,95 @@ def find_unique_sites(combined):
 if __name__ == "__main__":
     # read in variables from the config file
     config = configparser.ConfigParser()
-    config.read(Path(__file__).parent.parent.parent / 'config.ini')
+    #config.read(Path(__file__).parent.parent.parent / 'config.ini')
+    config.read(sys.argv[1] + '../../config.ini')
     NO_REGIONS = int(config["inversion_constants"]["no_regions"])
     CASE = config["inversion_constants"]["case"]
     OBSPACK_DIR = Path(config["paths"]["obspack_dir"])
     GEOSOUT_DIR = Path(config["paths"]["geos_out"])
     PERTURB_START = pd.to_datetime(config["dates"]["perturb_start"])
+    SPINUP_START = pd.to_datetime(config["dates"]["spinup_start"])
 
     # read in observations
     print("Reading in obs...")
-    obs_files = list(OBSPACK_DIR.glob(f"obspack_n2o.{PERTURB_START.year}*.nc"))
+    obs_files = list(set(OBSPACK_DIR.glob("obspack_n2o.*.nc")) - set(OBSPACK_DIR.glob(f"obspack_n2o.{SPINUP_START.year}*.nc")))
     obs_files.sort()
     with xr.open_mfdataset(obs_files) as load:
         obspack_obs = load.load()
 
     # read in geoschem output in each directory
-    for output_dir in GEOSOUT_DIR.iterdir():
+    geoschem_out_dirs = list(GEOSOUT_DIR.iterdir())
+    iterator = int(sys.argv[2])
+    print(iterator)
+    output_dir = geoschem_out_dirs[iterator]
+    # for output_dir in GEOSOUT_DIR.iterdir():
         
-        print(output_dir)
+    print(output_dir)
 
-        print("Reading in geos...")
-        geos_files = list(output_dir.glob(f"GEOSChem.ObsPack.{PERTURB_START.year}*_0000z.nc4"))
-        geos_files.sort()
-        with xr.open_mfdataset(geos_files, preprocess=obspack_geos_preprocess) as load:
-            obspack_geos = load.load()
+    print("Reading in geos...")
+    geos_files = list(set(output_dir.glob("GEOSChem.ObsPack.*_0000z.nc4")) - set(output_dir.glob(f"GEOSChem.ObsPack.{SPINUP_START.year}*_0000z.nc4")))
+    geos_files.sort()
+    with xr.open_mfdataset(geos_files, preprocess=obspack_geos_preprocess) as load:
+        obspack_geos = load.load()
 
-        # combine the two datasets
-        print("Combining datasets...")
-        combined = xr.merge([obspack_obs[["latitude", "longitude", "altitude",
-                                            "time", "obspack_id", "value", 
-                                            "value_unc", "network"]],
-                                obspack_geos])
-        combined = combined.rename({"latitude":"obs_lat", "longitude":"obs_lon",
-                                    "altitude":"obs_alt", "time":"obs_time", 
-                                    "value":"obs_value", "value_unc":"obs_value_unc"})
+    # combine the two datasets
+    print("Combining datasets...")
+    combined = xr.merge([obspack_obs[["latitude", "longitude", "altitude",
+                                        "time", "obspack_id", "value", 
+                                        "value_unc", "network"]],
+                            obspack_geos])
+    combined = combined.rename({"latitude":"obs_lat", "longitude":"obs_lon",
+                                "altitude":"obs_alt", "time":"obs_time", 
+                                "value":"obs_value", "value_unc":"obs_value_unc"})
 
-        # find uniqe sites
-        print("Finding unique sites...")
-        list_of_sites, unique_sites = find_unique_sites(combined)
+    # find uniqe sites
+    print("Finding unique sites...")
+    list_of_sites, unique_sites = find_unique_sites(combined)
 
-        # make dimensions site and time
-        print("Sorting out dims...")
-        combined = combined.assign_coords(obs_time=combined["obs_time"])
-        combined["site"] = (("obs"), np.array(list_of_sites))
-        combined = combined.swap_dims({"obs":"obs_time"})
+    # make dimensions site and time
+    print("Sorting out dims...")
+    combined = combined.assign_coords(obs_time=combined["obs_time"])
+    combined["site"] = (("obs"), np.array(list_of_sites))
+    combined = combined.swap_dims({"obs":"obs_time"})
 
-        # create monthly mean for each site
-        print("Making monthly mean...")
-        resampled_sites = []
-        for site in unique_sites:
-            onesite = combined.where(combined["site"] == site, drop=True)
-            onesite["obs_value_unc"] = onesite["obs_value_unc"] ** 2
-            onesite_resampled = onesite.resample(obs_time="M").mean()
-            no_samples = onesite.resample(obs_time="M").count()
-            onesite_resampled["obs_value_unc"] = np.sqrt(onesite_resampled["obs_value_unc"]) / np.sqrt(no_samples["obs_value_unc"])
-            resampled_sites.append(onesite_resampled)
+    # create monthly mean for each site
+    print("Making monthly mean...")
+    resampled_sites = []
+    for site in unique_sites:
+        onesite = combined.where(combined["site"] == site, drop=True)
+        onesite["obs_value_unc"] = onesite["obs_value_unc"] ** 2
+        onesite_resampled = onesite.resample(obs_time="M").mean()
+        no_samples = onesite.resample(obs_time="M").count()
+        onesite_resampled["obs_value_unc"] = np.sqrt(onesite_resampled["obs_value_unc"]) / np.sqrt(no_samples["obs_value_unc"])
+        resampled_sites.append(onesite_resampled)
 
-        # recombine sites
-        print("Recombining sites...")
-        site_combined = xr.concat(resampled_sites, dim="site")
-        site_combined["site"] = (("site"), unique_sites)
+    # recombine sites
+    print("Recombining sites...")
+    site_combined = xr.concat(resampled_sites, dim="site")
+    site_combined["site"] = (("site"), unique_sites)
 
-        # generate new obspack_id
-        print("Making new obspack_id...")
-        site_combined["obspack_id"] = xr.zeros_like(site_combined["obs_value"], dtype='<U200')
-        for i, site in enumerate(site_combined["site"].values):
-            for j, time in enumerate(site_combined["obs_time"].values):
-                site_combined["obspack_id"][i, j] = monthly_mean_obspack_id(site, time)
+    # generate new obspack_id
+    print("Making new obspack_id...")
+    site_combined["obspack_id"] = xr.zeros_like(site_combined["obs_value"], dtype='<U200')
+    for i, site in enumerate(site_combined["site"].values):
+        for j, time in enumerate(site_combined["obs_time"].values):
+            site_combined["obspack_id"][i, j] = monthly_mean_obspack_id(site, time)
 
-        # sum different regions if base 
-        if str(output_dir)[-4:] == CASE:
-            # sum up different regions
-            site_combined["CH4_sum"] = xr.zeros_like(site_combined["CH4_R00"])
-            for i in range(0, NO_REGIONS+1):
-                site_combined["CH4_sum"] += site_combined[f"CH4_R{i:02d}"]
+    # sum different regions if base 
+    if str(output_dir)[-4:] == CASE:
+        # sum up different regions
+        site_combined["CH4_sum"] = xr.zeros_like(site_combined["CH4_R00"])
+        for i in range(0, NO_REGIONS+1):
+            site_combined["CH4_sum"] += site_combined[f"CH4_R{i:02d}"]
+    else:
+        # in perturbed runs, all months before perturbation shouldn't exist
+        # some do without this code because the obspack contains 23:55-23:59 of the 
+        # last day of the month
+        year = int(str(output_dir)[-6:-2])
+        month = int(str(output_dir)[-2:])
+        site_combined = site_combined.where(site_combined["obs_time"] >= np.datetime64(f"{year}-{month:02d}-01"))
 
-        # save combined file
-        site_combined.to_netcdf(output_dir / "combined_mf.nc")
+
+
+    # save combined file
+    site_combined.to_netcdf(output_dir / "combined_mf.nc")
