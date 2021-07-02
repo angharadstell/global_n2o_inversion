5ca145ea955d154680ba41fbf59512bd786ab97b
Angharad Stell <as16992@bristol.ac.uk>
2021-07-02 14:52:24 +0100

    fix flag test

--- tests/obs/test_agage_obs.py
+++ tests/obs/test_agage_obs.py
@@ -21,11 +21,10 @@ def test_create_obspack_id():
         b"obspack_multi-species_1_AGAGEInSitu_v1.0_2012-02-29~n2o_abc_surface-insitu_1_agage_Event~999"
 
 def test_create_noaa_style_flag():
-    status_flag = np.array([0, 1, 0, 1])
-    integration_flag = np.array([0, 0, 1, 1])
-    correct_answer = [b"...", b"a..", b".a.", b"aa."]
-    for i in range(4):
-        assert agage_obs.create_noaa_style_flag(status_flag[i], integration_flag[i]) == correct_answer[i]
+    status_flag = np.array([0, 1])
+    correct_answer = [b"...", b"a.."]
+    for i in range(len(status_flag)):
+        assert agage_obs.create_noaa_style_flag(status_flag[i]) == correct_answer[i]
 
 def test_datetime_to_unix_zero():
     assert agage_obs.datetime_to_unix(np.datetime64("1970-01-01T00:00:00")) == 0
