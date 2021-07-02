39a7db776c1c0e240c8f726cb33ba467a04011da
Angharad Stell <as16992@bristol.ac.uk>
2021-07-02 15:02:42 +0100

    check all geoschemruns completed

---
+++ n2o_inv/perturbed_runs/check_success.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# load variables
+source ../spinup/bash_var.sh
+
+# iterate through each perturbed case
+for YEAR in $(eval echo "{${dates[perturb_start]:0:4}..$((${dates[perturb_end]:0:4}-1))}")
+do
+    for MONTH in {1..12}
+    do
+        # construct peturbed file location
+        perturb_case="$YEAR$(printf '%02d' $MONTH)"
+        echo $perturb_case
+
+        tail -1 ${paths[geo_rundirs]}/$perturb_case/GC.log
+    done
+done
