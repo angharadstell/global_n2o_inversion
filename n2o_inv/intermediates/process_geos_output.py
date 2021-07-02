8d9a13775e548213250ac116769f4206338d61ac
Angharad Stell <as16992@bristol.ac.uk>
2021-07-02 14:36:01 +0100

    remove unwanted obs sites, diff way of calculating obs err

--- n2o_inv/intermediates/process_geos_output.py
+++ n2o_inv/intermediates/process_geos_output.py
@@ -16,7 +16,7 @@ def monthly_mean_obspack_id(site, date):
     obspack_id = f"obspack_multi-species_1_CCGGSurfaceFlask_v2.0_2021-02-09~n2o_{site}_surface-flask_1_ccgg_Event~{identifier}"
     return obspack_id
 
-def obspack_geos_preprocess(ds):
+def obspack_geos_preprocess(ds, obspack_obs, no_regions):
     """
     Preprocess geoschem output for reading into xarray: make coord monotonically increase,
     drop unwanted vars, and turn mf to ppb.
@@ -34,12 +34,41 @@ def obspack_geos_preprocess(ds):
     else:
         raise ValueError("obspack obs and geoschem values don't align")
     
-    wanted_var = [f"CH4_R{region:02d}" for region in range(0, NO_REGIONS+1)]
+    wanted_var = [f"CH4_R{region:02d}" for region in range(0, no_regions+1)]
     ds = ds[wanted_var]
     
     ds = ds * 1e9
     
     return ds
+
+def read_obs(obspack_dir, spinup_start, perturb_end, final_end):
+    all_obs_files = set(obspack_dir.glob("obspack_n2o.*.nc"))
+    unwanted_obs_files = list(obspack_dir.glob(f"obspack_n2o.{spinup_start.year}*.nc"))
+    if final_end != perturb_end:
+        for year in range(perturb_end.year, final_end.year):
+            unwanted_obs_files = unwanted_obs_files + list(obspack_dir.glob(f"obspack_n2o.{year}*.nc"))
+
+    obs_files = list(all_obs_files - set(unwanted_obs_files))
+    obs_files.sort()
+    with xr.open_mfdataset(obs_files) as load:
+        obspack_obs = load.load()
+    return obspack_obs
+
+def read_geos(output_dir, spinup_start, obspack_obs, no_regions):
+    geos_files = list(set(output_dir.glob("GEOSChem.ObsPack.*_0000z.nc4")) - set(output_dir.glob(f"GEOSChem.ObsPack.{spinup_start.year}*_0000z.nc4")))
+    geos_files.sort()
+
+    # if first day of year end pressent, remove
+    print("checking last file to be read in")
+    if "0101_0000z.nc4" in str(geos_files[-1]):
+        geos_files = geos_files[:-1]
+    # check stopping in right place
+    print(geos_files[-1])
+
+    with xr.open_mfdataset(geos_files, 
+                           preprocess=lambda ds: obspack_geos_preprocess(ds, obspack_obs, no_regions)) as load:
+        obspack_geos = load.load()
+    return obspack_geos
 
 def find_unique_sites(combined):
     """
@@ -63,32 +92,27 @@ if __name__ == "__main__":
     config.read(sys.argv[1] + '/../../config.ini')
     NO_REGIONS = int(config["inversion_constants"]["no_regions"])
     CASE = config["inversion_constants"]["case"]
+    AGAGE_SITES = config["inversion_constants"]["agage_sites"].split(",")
     OBSPACK_DIR = Path(config["paths"]["obspack_dir"])
     GEOSOUT_DIR = Path(config["paths"]["geos_out"])
     PERTURB_START = pd.to_datetime(config["dates"]["perturb_start"])
     SPINUP_START = pd.to_datetime(config["dates"]["spinup_start"])
+    PERTURB_END = pd.to_datetime(config["dates"]["perturb_end"])
+    FINAL_END = pd.to_datetime(config["dates"]["final_end"])
 
     # read in observations
     print("Reading in obs...")
-    obs_files = list(set(OBSPACK_DIR.glob("obspack_n2o.*.nc")) - set(OBSPACK_DIR.glob(f"obspack_n2o.{SPINUP_START.year}*.nc")))
-    obs_files.sort()
-    with xr.open_mfdataset(obs_files) as load:
-        obspack_obs = load.load()
+    obspack_obs = read_obs(OBSPACK_DIR, SPINUP_START, PERTURB_END, FINAL_END)
 
     # read in geoschem output in each directory
     geoschem_out_dirs = list(GEOSOUT_DIR.iterdir())
     iterator = int(sys.argv[2])
     print(iterator)
     output_dir = geoschem_out_dirs[iterator]
-    # for output_dir in GEOSOUT_DIR.iterdir():
-        
     print(output_dir)
 
     print("Reading in geos...")
-    geos_files = list(set(output_dir.glob("GEOSChem.ObsPack.*_0000z.nc4")) - set(output_dir.glob(f"GEOSChem.ObsPack.{SPINUP_START.year}*_0000z.nc4")))
-    geos_files.sort()
-    with xr.open_mfdataset(geos_files, preprocess=obspack_geos_preprocess) as load:
-        obspack_geos = load.load()
+    obspack_geos = read_geos(output_dir, SPINUP_START, obspack_obs, NO_REGIONS)
 
     # combine the two datasets
     print("Combining datasets...")
@@ -110,15 +134,32 @@ if __name__ == "__main__":
     combined["site"] = (("obs"), np.array(list_of_sites))
     combined = combined.swap_dims({"obs":"obs_time"})
 
+    # drop air sites
+    combined = combined.where(~combined["site"].str.contains("NOAAair"), drop=True)
+
+    # drop NOAA sites where we have AGAGE data
+    noaa_sites_to_drop = [site.lower() + "NOAAsurf" for site in AGAGE_SITES]
+    for site in noaa_sites_to_drop:
+        combined = combined.where(combined["site"] != site, drop=True)
+    _, unique_sites = find_unique_sites(combined)
+    # rescale AGAGE to NOAA
+    agage_mask = combined["network"] == "AGAGEsurf"
+    agage_over_noaa_ratio = pd.read_csv(OBSPACK_DIR / "agage_noaa_scaling/agage_over_noaa_ratio.csv", index_col=0).iloc[0].values[0]
+    combined["obs_value"][agage_mask] = combined["obs_value"][agage_mask] / agage_over_noaa_ratio
+
     # create monthly mean for each site
     print("Making monthly mean...")
     resampled_sites = []
     for site in unique_sites:
         onesite = combined.where(combined["site"] == site, drop=True)
-        onesite["obs_value_unc"] = onesite["obs_value_unc"] ** 2
         onesite_resampled = onesite.resample(obs_time="M").mean()
-        no_samples = onesite.resample(obs_time="M").count()
-        onesite_resampled["obs_value_unc"] = np.sqrt(onesite_resampled["obs_value_unc"]) / np.sqrt(no_samples["obs_value_unc"])
+        onesite_resampled["obs_value_unc"] = onesite["obs_value"].resample(obs_time="M").std()
+        # if only one point, std is nan...
+        onesite_resampled["obs_value_unc"] = onesite_resampled["obs_value_unc"].where(~xr.ufuncs.isnan(onesite_resampled["obs_value_unc"]), 
+                                                                                      onesite["obs_value_unc"].median())
+        # if uncertainty is zero, e.g. two identical measurements, take typical value...
+        onesite_resampled["obs_value_unc"] = onesite_resampled["obs_value_unc"].where(onesite_resampled["obs_value_unc"] != 0, 
+                                                                                      onesite["obs_value_unc"].median())
         resampled_sites.append(onesite_resampled)
 
     # recombine sites
