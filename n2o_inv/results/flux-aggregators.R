de21214a4c6415cf807b5306933ef799214e31ba
Angharad Stell <as16992@bristol.ac.uk>
2021-08-23 17:00:40 +0100

    make testable raster

--- n2o_inv/results/flux-aggregators.R
+++ n2o_inv/results/flux-aggregators.R
@@ -6,6 +6,8 @@ library(ncdf4)
 
 source(Sys.getenv('INVERSION_BASE_PARTIAL'))
 
+#source('/home/as16992/wombat-paper/3_inversion/src/partials/base.R')
+
 options(dplyr.summarise.inform = FALSE)
 
 ###############################################################################
@@ -18,156 +20,176 @@ args <- arg_parser('', hide.opts = TRUE) %>%
   add_argument('--output', '') %>%
   parse_args()
 
+# args <- vector(mode = "list", length = 3)
+# names(args) <- c('process_model', 'transcom_mask', 'output')
+# args$process_model <- '/work/as16992/geoschem/N2O/intermediates/process-model.rds'
+# args$transcom_mask <- '/work/chxmr/shared/TRANSCOM/TRANSCOM3_basis_functions/TRANSCOM_Map_GEOSCHEM.nc'
+# args$output <- '/work/as16992/geoschem/N2O/results/flux-aggregators.rds'
+
+###############################################################################
+# FUNCTIONS
+###############################################################################
+
+process_transcom_regions <- function(transcome_mask_file) {
+  log_info('Loading Transcom mask')
+  # NOT CONVINCED THIS IS 100% RIGHT - NEEDS A TEST
+  transcom_mask <- raster(args$transcom_mask, stopIfNotEqualSpaced = FALSE)
+
+}
+
+
 ###############################################################################
 # EXECUTION
 ###############################################################################
 
-log_info('Loading process model')
-process_model <- readRDS(args$process_model)
+main <- function() {
+  log_info('Loading process model')
+  process_model <- readRDS(args$process_model)
 
-log_info('Loading Transcom mask')
-fn <- ncdf4::nc_open(args$transcom_mask)
-v <- function(...) ncdf4::ncvar_get(fn, ...)
-transcom_mask <- raster(nrows=72,
-                        ncols=46,
-                        vals=v('regions'))
+  # process transcom regions
+  transcom_mask <- process_transcom_regions(args$transcom_mask)
 
-log_info('Calculating')
-process_model$control_emissions <- process_model$control_emissions %>%
-  mutate(
-    transcom_region = extract(transcom_mask, cbind(longitude, latitude))
-  ) %>%
-  mutate(
-    transcom_region = if_else(is.na(transcom_region), 0, transcom_region)
-  )
+  log_info('Calculating')
+  process_model$control_emissions <- process_model$control_emissions %>%
+    mutate(
+      transcom_region = extract(transcom_mask, cbind(longitude, latitude))
+    ) %>%
+    mutate(
+      transcom_region = if_else(is.na(transcom_region), 0, transcom_region)
+    )
 
-emission_groups <- c(list(
-  list(
-    name = 'Global',
-    expr = quote(type %in% c('land', 'ocean') & latitude > -Inf)
-  ),
-  list(
-    name = 'Global land',
-    expr = quote(type == 'land')
-  ),
-  list(
-    name = 'Global oceans',
-    expr = quote(type == 'ocean')
-  ),
-  list(
-    name = 'N extratropics (23.5 - 90)',
-    expr = quote(type %in% c('land', 'ocean') & latitude > 23.5)
-  ),
-  list(
-    name = 'S extratropics (-90 - -23.5)',
-    expr = quote(type %in% c('land', 'ocean') & latitude < -23.5)
-  ),
-  list(
-    name = 'N polar (65 - 90)',
-    expr = quote(type %in% c('land', 'ocean') & latitude > 65)
-  ),
-  list(
-    name = 'N mid (23.5 - 65)',
-    expr = quote(type %in% c('land', 'ocean') & between(latitude, 23.5, 65))
-  ),
-  list(
-    name = 'N tropics (0 - 23.5)',
-    expr = quote(type %in% c('land', 'ocean') & between(latitude, 0, 23.4))
-  ),
-  list(
-    name = 'S tropics (-23.5 - 0)',
-    expr = quote(type %in% c('land', 'ocean') & between(latitude, -23.5, 0))
-  ),
-  list(
-    name = 'S mid (-65 - -23.5)',
-    expr = quote(type %in% c('land', 'ocean') & between(latitude, -65, -23.5))
-  ),
-  list(
-    name = 'S polar (-90 - -65)',
-    expr = quote(type %in% c('land', 'ocean') & latitude < -65)
-  ),
-  list(
-    name = 'N upper (45 - 90)',
-    expr = quote(type %in% c('land', 'ocean') & latitude > 45)
-  ),
-  list(
-    name = 'N lower (0 - 45)',
-    expr = quote(type %in% c('land', 'ocean') & between(latitude, 0, 45))
-  ),
-  list(
-    name = 'S upper (-45 - 0)',
-    expr = quote(type %in% c('land', 'ocean') & between(latitude, -45, 0))
-  ),
-  list(
-    name = 'S lower (-90 - -45)',
-    expr = quote(type %in% c('land', 'ocean') & latitude < -45)
-  ),
-  list(
-    name = 'N polar land (65 - 90)',
-    expr = quote(type == 'land' & latitude > 65)
-  ),
-  list(
-    name = 'N mid land (23.5 - 65)',
-    expr = quote(type == 'land' & between(latitude, 23.5, 65))
-  ),
-  list(
-    name = 'N tropics land (0 - 23.5)',
-    expr = quote(type == 'land' & between(latitude, 0, 23.4))
-  ),
-  list(
-    name = 'S tropics land (-23.5 - 0)',
-    expr = quote(type == 'land' & between(latitude, -23.5, 0))
-  ),
-  list(
-    name = 'S mid land (-65 - -23.5)',
-    expr = quote(type == 'land' & between(latitude, -65, -23.5))
-  ),
-  list(
-    name = 'S polar land (-90 - -65)',
-    expr = quote(type == 'land' & latitude < -65)
-  ),
-  list(
-    name = 'N polar oceans (65 - 90)',
-    expr = quote(type == 'ocean' & latitude > 65)
-  ),
-  list(
-    name = 'N mid oceans (23.5 - 65)',
-    expr = quote(type == 'ocean' & between(latitude, 23.5, 65))
-  ),
-  list(
-    name = 'N tropics oceans (0 - 23.5)',
-    expr = quote(type == 'ocean' & between(latitude, 0, 23.4))
-  ),
-  list(
-    name = 'S tropics oceans (-23.5 - 0)',
-    expr = quote(type == 'ocean' & between(latitude, -23.5, 0))
-  ),
-  list(
-    name = 'S mid oceans (-65 - -23.5)',
-    expr = quote(type == 'ocean' & between(latitude, -65, -23.5))
-  ),
-  list(
-    name = 'S polar oceans (-90 - -65)',
-    expr = quote(type == 'ocean' & latitude < -65)
-  )
-), lapply(process_model$regions, function(region) {
-  list(
-    name = sprintf('T%02d', region),
-    expr = parse(text = sprintf("type %%in%% c('land', 'ocean') & transcom_region == %d", region))[[1]]
-  )
-}))
+  emission_groups <- c(list(
+    list(
+      name = 'Global',
+      expr = quote(type %in% c('land', 'ocean') & latitude > -Inf)
+    ),
+    list(
+      name = 'Global land',
+      expr = quote(type == 'land')
+    ),
+    list(
+      name = 'Global oceans',
+      expr = quote(type == 'ocean')
+    ),
+    list(
+      name = 'N extratropics (23.5 - 90)',
+      expr = quote(type %in% c('land', 'ocean') & latitude > 23.5)
+    ),
+    list(
+      name = 'S extratropics (-90 - -23.5)',
+      expr = quote(type %in% c('land', 'ocean') & latitude < -23.5)
+    ),
+    list(
+      name = 'N polar (65 - 90)',
+      expr = quote(type %in% c('land', 'ocean') & latitude > 65)
+    ),
+    list(
+      name = 'N mid (23.5 - 65)',
+      expr = quote(type %in% c('land', 'ocean') & between(latitude, 23.5, 65))
+    ),
+    list(
+      name = 'N tropics (0 - 23.5)',
+      expr = quote(type %in% c('land', 'ocean') & between(latitude, 0, 23.4))
+    ),
+    list(
+      name = 'S tropics (-23.5 - 0)',
+      expr = quote(type %in% c('land', 'ocean') & between(latitude, -23.5, 0))
+    ),
+    list(
+      name = 'S mid (-65 - -23.5)',
+      expr = quote(type %in% c('land', 'ocean') & between(latitude, -65, -23.5))
+    ),
+    list(
+      name = 'S polar (-90 - -65)',
+      expr = quote(type %in% c('land', 'ocean') & latitude < -65)
+    ),
+    list(
+      name = 'N upper (45 - 90)',
+      expr = quote(type %in% c('land', 'ocean') & latitude > 45)
+    ),
+    list(
+      name = 'N lower (0 - 45)',
+      expr = quote(type %in% c('land', 'ocean') & between(latitude, 0, 45))
+    ),
+    list(
+      name = 'S upper (-45 - 0)',
+      expr = quote(type %in% c('land', 'ocean') & between(latitude, -45, 0))
+    ),
+    list(
+      name = 'S lower (-90 - -45)',
+      expr = quote(type %in% c('land', 'ocean') & latitude < -45)
+    ),
+    list(
+      name = 'N polar land (65 - 90)',
+      expr = quote(type == 'land' & latitude > 65)
+    ),
+    list(
+      name = 'N mid land (23.5 - 65)',
+      expr = quote(type == 'land' & between(latitude, 23.5, 65))
+    ),
+    list(
+      name = 'N tropics land (0 - 23.5)',
+      expr = quote(type == 'land' & between(latitude, 0, 23.4))
+    ),
+    list(
+      name = 'S tropics land (-23.5 - 0)',
+      expr = quote(type == 'land' & between(latitude, -23.5, 0))
+    ),
+    list(
+      name = 'S mid land (-65 - -23.5)',
+      expr = quote(type == 'land' & between(latitude, -65, -23.5))
+    ),
+    list(
+      name = 'S polar land (-90 - -65)',
+      expr = quote(type == 'land' & latitude < -65)
+    ),
+    list(
+      name = 'N polar oceans (65 - 90)',
+      expr = quote(type == 'ocean' & latitude > 65)
+    ),
+    list(
+      name = 'N mid oceans (23.5 - 65)',
+      expr = quote(type == 'ocean' & between(latitude, 23.5, 65))
+    ),
+    list(
+      name = 'N tropics oceans (0 - 23.5)',
+      expr = quote(type == 'ocean' & between(latitude, 0, 23.4))
+    ),
+    list(
+      name = 'S tropics oceans (-23.5 - 0)',
+      expr = quote(type == 'ocean' & between(latitude, -23.5, 0))
+    ),
+    list(
+      name = 'S mid oceans (-65 - -23.5)',
+      expr = quote(type == 'ocean' & between(latitude, -65, -23.5))
+    ),
+    list(
+      name = 'S polar oceans (-90 - -65)',
+      expr = quote(type == 'ocean' & latitude < -65)
+    )
+  ), lapply(process_model$regions, function(region) {
+    list(
+      name = sprintf('T%02d', region),
+      expr = parse(text = sprintf("type %%in%% c('land', 'ocean') & transcom_region == %d", region))[[1]]
+    )
+  }))
 
-log_debug('Computing aggregators')
-aggregators <- lapply(emission_groups, function(emission_group) {
-  log_trace('Processing {emission_group$name}')
-  emission_group$aggregator <- flux_aggregator(
-    process_model,
-    !! emission_group$expr
-  )
-  emission_group
-})
+  log_debug('Computing aggregators')
+  aggregators <- lapply(emission_groups, function(emission_group) {
+    log_trace('Processing {emission_group$name}')
+    emission_group$aggregator <- flux_aggregator(
+      process_model,
+      !! emission_group$expr
+    )
+    emission_group
+  })
 
-log_info('Saving')
-saveRDS(aggregators, args$output)
+  log_info('Saving')
+  saveRDS(aggregators, args$output)
 
-log_info('Done')
+  log_info('Done')
+}
+
+if (getOption('run.main', default=TRUE)) {
+   main()
+}
---
+++ tests/results/test_flux_aggregators.R
@@ -0,0 +1,36 @@
+here::i_am("tests/results/test_flux_aggregators.R")
+
+library(testthat)
+library(here)
+
+options(run.main = FALSE)
+source(paste0(here(), "/n2o_inv/results/flux-aggregators.R"), chdir = TRUE)
+
+# cant use Rscript -e "testthat::test_dir('tests/results')" to run tests without hard coding, confused
+config <- read.ini(paste0(here(), "/config.ini"))
+
+# set up transcom regions
+transcom_file <- config$inversion_constants$geo_transcom_mask
+
+# read in transcom as raster object
+transcom_raster <- process_transcom_regions(transcom_file)
+
+# read in transcom regions using netcdf
+transcom_nc <- ncdf4::nc_open(transcom_file)
+v <- function(...) ncdf4::ncvar_get(transcom_nc, ...)
+
+# check that the raster matches the original value in the netcdf file for the geoschem grid
+
+# check each latitude
+for (i in 1:length(v("lat"))) {
+    test_that(paste(v("lat")[i], "lat"), {
+        expect_equal(extract(transcom_raster, cbind(v("lon"), rep(v("lat")[i], length(v("lon"))))), v("regions")[, i])
+    })
+}
+
+# check each longitude
+for (i in 1:length(v("lon"))) {
+    test_that(paste(v("lon")[i], "lon"), {
+        expect_equal(extract(transcom_raster, cbind(rep(v("lon")[i], length(v("lat"))), v("lat"))), v("regions")[i, ])
+    })
+}
