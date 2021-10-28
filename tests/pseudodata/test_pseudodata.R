21994e544e388485f48c738f21718ffd011b65a5
Angharad Stell <as16992@bristol.ac.uk>
2021-10-28 16:41:58 +0100

    correct number of args for new alpha_to_obs function

--- tests/pseudodata/test_pseudodata.R
+++ tests/pseudodata/test_pseudodata.R
@@ -11,10 +11,6 @@ config <- read.ini(paste0(here(), "/config.ini"))
 
 
 
-obs_matching <- match(
-observations$observation_id,
-control_mf$observation_id
-)
 
 # check that alpha is zero returns control_mf
 n_samples <- 2
@@ -25,15 +21,15 @@ n_obs <- dim(observations)[1]
 
 alpha_samples <- matrix(rep(0, n_samples * n_alpha), n_samples, n_alpha)
 test_that("check that alpha is zero returns control_mf",
-          expect_equal(alpha_to_obs(alpha_samples, 0) - control_mf$co2[obs_matching],
+          expect_equal(alpha_to_obs(alpha_samples, 0, control_mf, perturbations, sensitivities) - control_mf$co2,
                        matrix(rep(0, n_samples * n_obs), n_obs, n_samples),
                        check.attributes = FALSE))
 
 alpha_samples <- matrix(rep(1, n_samples * n_alpha), n_samples, n_alpha)
 test_that("alpha > 0 all mf should be higher than control_mf",
-          expect_true(all(alpha_to_obs(alpha_samples, 0) > control_mf$co2[obs_matching])))
+          expect_true(all(alpha_to_obs(alpha_samples, 0, control_mf, perturbations, sensitivities) > control_mf$co2)))
 
 alpha_samples <- matrix(rep(-1, n_samples * n_alpha), n_samples, n_alpha)
 test_that("alpha < 0 all mf should be lower than control_mf",
-          expect_true(all(alpha_to_obs(alpha_samples, 0) < control_mf$co2[obs_matching])))
+          expect_true(all(alpha_to_obs(alpha_samples, 0, control_mf, perturbations, sensitivities) < control_mf$co2)))
 
