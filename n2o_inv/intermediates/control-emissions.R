8616654119bdc2988bfedb2efaa49db5c3b7d7db
Angharad Stell <as16992@bristol.ac.uk>
2021-11-11 15:28:55 +0000

    add more comments

--- n2o_inv/intermediates/control-emissions.R
+++ n2o_inv/intermediates/control-emissions.R
@@ -7,29 +7,29 @@ library(ncdf4)
 library(tidyr)
 library(wombat)
 
-
+###############################################################################
+# GLOBAL CONSTANTS
+###############################################################################
+# Read in command line arguments
 args <- arg_parser('', hide.opts = TRUE) %>%
   add_argument('--flux-file', '') %>%
   add_argument('--output', '') %>%
   parse_args()
 
-###############################################################################
-# GLOBAL CONSTANTS
-###############################################################################
+# Read in config file
 config <- read.ini(paste0(here(), "/config.ini"))
-
-case <- config$inversion_constants$case
-# locations of files
-geos_out_dir <- config$paths$geos_out
-inte_out_dir <- config$paths$geos_inte
 
 ###############################################################################
 # FUNCTIONS
 ###############################################################################
 
 sum_ch4_tracers <- function(v, region_start, region_end) {
-  # sum up the tracers for each perturbed run
+  # Sum up the emissions for each desired region in geoschem, outputting an 
+  # array of their distribution in space and time.
+
+  # Create an array of zeros that matches the shape of "EMIS_CH4_R00"
   total_ch4 <- array(rep(0, length(v("EMIS_CH4_R00"))), dim(v("EMIS_CH4_R00")))
+  # Iterate over desired regions
   for (region in region_start:region_end) {
     total_ch4 <- total_ch4 + v(sprintf("EMIS_CH4_R%02d", region))
   }
@@ -40,13 +40,20 @@ sum_ch4_tracers <- function(v, region_start, region_end) {
 # EXECUTION
 ###############################################################################
 
-fn <- nc_open(sprintf("%s/%s/%s", geos_out_dir, case, args$flux_file))
+# Read in flux file for the base run
+fn <- nc_open(sprintf("%s/%s/%s", 
+                      config$paths$geos_out,
+                      config$inversion_constants$case,
+                      args$flux_file))
 v <- function(...) ncdf4::ncvar_get(fn, ...)
 
-n_longitudes <- length(v("longitude"))
-n_latitudes <- length(v("latitude"))
-month_starts <- as.Date(ncvar_get_time(fn, "time"))
+# Extract some constants from the file
+n_longitudes <- length(v("longitude"))              # Number of longitudes in model
+n_latitudes <- length(v("latitude"))                # Number of latitudes in model
+month_starts <- as.Date(ncvar_get_time(fn, "time")) # The months starts in model
 
+# Create a model_id that is unique for each grid cell and month
+# Also extract the area of each grid cell from geoschem
 locations <- expand.grid(
   longitude_index = seq(1, n_longitudes),
   latitude_index = seq(1, n_latitudes),
@@ -58,19 +65,26 @@ locations <- expand.grid(
 ) %>%
   select(month_start, model_id, everything())
 
-
+# Read in the number of transcom regions (and how many of those are land) from the config file.
+# The distinction between land and ocean regions only matters if you want to parameterise the
+# land and ocean differently in the inversion, as in the original WOMBAT paper.
 no_regions <- as.numeric(config$inversion_constants$no_regions)
 no_land_regions <- as.numeric(config$inversion_constants$no_land_regions)
 
+# Read in the emissions from geoschem, and add to the data frame
 emissions <- cbind(locations, data.frame(
-  # Comes in kg/m^2/s
+  # Emissions come in as kg/m^2/s from geoschem
   land = as.vector(sum_ch4_tracers(v, 0, (no_land_regions - 1))),
   ocean = as.vector(sum_ch4_tracers(v, no_land_regions, no_regions))
 ))
-  
+
+# Reshape data so the land/ocean columns are combined to one value column (flux_density),
+# and one category column (type)
 emissions <- pivot_longer(emissions, c(land, ocean),
                           names_to = "type", values_to = "flux_density")
 
+# Add in more information on the grid cells - longitude centre, width of the cell,
+# latitude centre, height of cell
 emissions <- emissions %>% left_join(
   data.frame(
     longitude_index = seq(1, n_longitudes),
@@ -89,4 +103,5 @@ left_join(
 ) %>%
 select(-longitude_index, -latitude_index)
 
-fst::write_fst(emissions, sprintf("%s/%s", inte_out_dir, args$output))
+# Save control emissions intermediate
+fst::write_fst(emissions, sprintf("%s/%s", config$paths$geos_inte, args$output))
--- n2o_inv/intermediates/perturbations.R
+++ n2o_inv/intermediates/perturbations.R
@@ -8,28 +8,18 @@ library(ncdf4)
 library(tidyr)
 library(wombat)
 
+###############################################################################
+# GLOBAL CONSTANTS
+###############################################################################
+# Read in command line arguments
 args <- arg_parser('', hide.opts = TRUE) %>%
   add_argument('--flux-file', '') %>%
   add_argument('--control-ems', '') %>%
   add_argument('--output', '') %>%
   parse_args()
 
-###############################################################################
-# GLOBAL CONSTANTS
-###############################################################################
+# Read in config file
 config <- read.ini(paste0(here(), "/config.ini"))
-
-no_regions <- as.numeric(config$inversion_constants$no_regions)
-no_land_regions <- as.numeric(config$inversion_constants$no_land_regions)
-case <- config$inversion_constants$case
-# locations of files
-geos_out_dir <- config$paths$geos_out
-inte_out_dir <- config$paths$geos_inte
-
-perturb_start <- as.Date(config$dates$perturb_start)
-perturb_end <- as.Date(config$dates$perturb_end)
-
-len_perturb <- as.numeric(config$inversion_constants$len_perturb)
 
 ###############################################################################
 # FUNCTIONS
@@ -39,9 +29,18 @@ sum_ch4_tracers <- function(v_base, v_pert,
                             region_start, region_end,
                             perturbed_region,
                             month_start, month_end) {
-  # sum up the tracers for each perturbed run
+  # Sum up the emissions for each desired region in geoschem, outputting an 
+  # array of their distribution in space and time. For each perturbed run, the 
+  # emissions are the base emissions summed over the not perturbed regions, added
+  # to the perturbed run emissions for the perturbed region
+
+  # Create an array of zeros that matches the shape of "EMIS_CH4_R00" in the base run
   total_ch4 <- array(rep(0, length(v_base("EMIS_CH4_R00"))), dim(v_base("EMIS_CH4_R00")))
+  # Iterate over desired regions
   for (region in region_start:region_end) {
+    # Add on the perturbed run emissions if the region is the perturbed region, else 
+    # add on the base run emissions.
+    # month_start and month_end needed because each perturbed run os only 2 years long
     if (region == perturbed_region) {
       total_ch4[, , month_start:month_end] <- total_ch4[, , month_start:month_end] + v_pert(sprintf("EMIS_CH4_R%02d", region))
     } else {
@@ -52,26 +51,32 @@ sum_ch4_tracers <- function(v_base, v_pert,
 }
 
 process_perturbation_part <- function(month, year, region) {
-  # Read in flux file
-  flux_file <- sprintf("%s/%s%02d/%s", geos_out_dir, year, month, args$flux_file)
+  # Extract the perturbed run emissions for a specific date and region, subtract the base run to 
+  # calculate perturbation emissions. The output is a nice data frame.
+
+  # Read in flux file for the perturbed run
+  flux_file <- sprintf("%s/%s%02d/%s", config$paths$geos_out, year, month, args$flux_file)
   print(flux_file)
   perturbed <- ncdf4::nc_open(flux_file)
   v <- function(...) ncdf4::ncvar_get(perturbed, ...)
 
-
+  # Extract grid cell info from gesochem
   locations <- expand.grid(
     longitude = v("longitude"),
     latitude = v("latitude"),
     month_start = as.Date(ncvar_get_time(fn, "time"))
   )
 
-  # bit of a hacky way to stop hitting index error - improve!
+  # Bit of a hacky way to stop hitting index error in sum_ch4_tracers
+  len_perturb <- as.numeric(config$inversion_constants$len_perturb)
   month_start <- (as.numeric(year) - first_year) * 12 + month
   month_end <- min(month_start + len_perturb - 1, length(unique(control$month_start)))
-
   print(month_start)
   print(month_end)
 
+  no_land_regions <- as.numeric(config$inversion_constants$no_land_regions)
+
+  # Work out perturbed run emissions, add to locations data frame
   as_tibble(cbind(locations, data.frame(
     region = region,
     from_month_start = lubridate::ymd(sprintf("%s-%02d-01", year, month)),
@@ -98,6 +103,7 @@ process_perturbation_part <- function(month, year, region) {
         ),
       by = c("month_start", "longitude", "latitude", "type")
     )  %>%
+    # subtract base run emissions from perturbed run emissions to get perturbations
     mutate(
       flux_density = flux_density - control_flux_density
     ) %>%
@@ -112,24 +118,27 @@ process_perturbation_part <- function(month, year, region) {
 
 }
 
-
-
-
-
 ###############################################################################
 # EXECUTION
 ###############################################################################
 
-control <- fst::read_fst(sprintf("%s/%s", inte_out_dir, args$control_ems))
+# Read in control emissions intermediate
+control <- fst::read_fst(sprintf("%s/%s", config$paths$geos_inte, args$control_ems))
 
-fn <- nc_open(sprintf("%s/%s/%s", geos_out_dir, case, args$flux_file))
+# Read in flux file for the base run
+fn <- nc_open(sprintf("%s/%s/%s", config$paths$geos_out, config$inversion_constants$case, args$flux_file))
 v_base <- function(...) ncdf4::ncvar_get(fn, ...)
 
+# Extract information on the dates in the model runs, as well as the number of regions
 unique_dates <- unique(control$month_start)
 first_year <- as.numeric(format(min(unique_dates), format = "%Y"))
 last_year <- as.numeric(format(max(unique_dates), format = "%Y"))
 no_years <- last_year - first_year + 1
 
+no_regions <- as.numeric(config$inversion_constants$no_regions)
+
+# Make vectors of all the different months and years that perturbed emissions need to
+# be calculated for
 if (no_years > 0) {
   months <- rep(rep(1:12, each = (no_regions + 1)), no_years)
   years <- rep(first_year:last_year, each = (12 * (no_regions + 1)))
@@ -138,12 +147,15 @@ if (no_years > 0) {
   years <- rep(first_year:last_year, each = (length(unique_dates) * (no_regions + 1)))
 }
 
+# Run process_perturbation_part for each month and region to collect all the 
+# perturbed emissions data
 perturbations <- mapply(function(m, y, r) process_perturbation_part(m, y, r),
                         months,
                         years,
                         rep(0:no_regions, length(unique_dates)),
                         SIMPLIFY = FALSE)
-
+# Recombine to one data frame
 perturbations_combined <- bind_rows(perturbations)
 
-fst::write_fst(perturbations_combined, sprintf("%s/%s", inte_out_dir, args$output))
+# Save the perturbations intermediate
+fst::write_fst(perturbations_combined, sprintf("%s/%s", config$paths$geos_inte, args$output))
