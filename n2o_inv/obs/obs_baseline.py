f83d6da4d7493d9ecb5ba4a8737875049ba5cd6e
Angharad Stell <as16992@bristol.ac.uk>
2021-12-01 09:30:19 +0000

    filter agage data with Alistair's baseline

--- n2o_inv/obs/obs_baseline.py
+++ n2o_inv/obs/obs_baseline.py
@@ -7,9 +7,24 @@ This module establishes which observations we want to keep.
 import configparser
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import xarray as xr
+
+def agage_baseline(site, time_vec):
+    # read in Alistair's baseline
+    filepath = Path(config["paths"]["data_dir"]) / "agage_baseline"
+    located_file = next(filepath.glob(f"NAME_baseline_{site[0:3].upper()}_*.nc"))
+
+    with xr.open_dataset(located_file) as load:
+        df = load.load()
+
+    # interpolate to measurement times
+    # take only points between two baseline points in Alistair's baseline
+    baseline = (df.interp(time = time_vec)["baseline_NAME"] == True).astype(int).values
+
+    return baseline
 
 def raw_obs_to_baseline(obspack_obs):
     # create place to store baseline
@@ -41,8 +56,21 @@ def raw_obs_to_baseline(obspack_obs):
         elif "NOAAair" in site:
             pass
         else:
-            # store baseline
-            obspack_obs["baseline"][obspack_obs["site"] == site] = 1
+            site_mask = obspack_obs["site"] == site
+            if "AGAGEsurf" in site:
+                obspack_obs["baseline"][site_mask] = agage_baseline(site, obspack_obs["time"][site_mask])
+                # plot to check it makes sense
+                fig, ax = plt.subplots()
+                scatter = ax.scatter(x=obspack_obs["time"][site_mask].values, y=obspack_obs["value"][site_mask], c=obspack_obs["baseline"][site_mask])
+                clegend = plt.legend(*scatter.legend_elements())
+                ax.add_artist(clegend)
+                plt.show()
+                # how much of difference?
+                print(obspack_obs["value"][site_mask].mean())
+                print(obspack_obs["value"][site_mask][obspack_obs["baseline"][site_mask] == 1].mean())
+            else:
+                # store baseline
+                obspack_obs["baseline"][site_mask] = 1
     
     return obspack_obs
 
