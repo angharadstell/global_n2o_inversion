d60cd1714a5983c262f028ca9268def8899397c1
Angharad Stell <as16992@bristol.ac.uk>
2022-02-04 12:59:12 +0000

    bring up to date with agage baseline and time filtering

--- n2o_inv/model_err/calc_model_err.py
+++ n2o_inv/model_err/calc_model_err.py
@@ -29,25 +29,39 @@ if __name__ == "__main__":
     FINAL_END = pd.to_datetime(config["dates"]["final_end"])
 
     # read in observations
+    # have to redo this (which was originally done in plot_obs.py and obs_baseline.py) because
+    # need to do it with the extra eight grid cells
     print("Reading in obs...")
     obspack_raw = process_geos_output.read_obs(OBSPACK_DIR / CASE, SPINUP_START, 
                                                FINAL_END, FINAL_END)
     print("Finding unique sites...")
     list_of_sites, unique_sites = process_geos_output.find_unique_sites(obspack_raw)
     obspack_raw["site"] = (("obs"), np.array(list_of_sites))
-    obspack_baseline = obs_baseline.raw_obs_to_baseline(obspack_raw)
+
+    # read in AGAGE baselines
+    agage_baseline_dict = obs_baseline.make_agage_baseline_dict(config)
+    # filter to only baseline
+    obspack_baseline = obs_baseline.raw_obs_to_baseline(obspack_raw, agage_baseline_dict)
+
+    # cut unwanted years
+    obspack_baseline = obspack_baseline.where(obspack_baseline["time"] >= pd.to_datetime(f"{PERTURB_START.year - 1}-12-31 23:55"), drop=True)
+    obspack_baseline = obspack_baseline.where(obspack_baseline["time"] < pd.to_datetime(f"{FINAL_END.year}-12-31 23:55"), drop=True)
+
+
 
     # read in geos output
+    # have to redo this (which was originally done in process_geos_output.py) because
+    # need to do it with the extra eight grid cells
     obspack_geos = process_geos_output.read_geos(GEOS_OUT / CASE, obspack_baseline,
                                                  NO_REGIONS, PERTURB_START.year, (PERTURB_END.year-1))
 
     # sum up different regions
     obspack_geos = plot_obs.add_ch4(obspack_geos, NO_REGIONS+1)
-
+    # obs_floor will be the same for each group of 9 gridcells
     obspack_geos["obs_floor"] = np.floor(obspack_geos["obs"])
 
 
-    # combine the two datasets
+    # combine the two datasets as in process_geos_output.py
     print("Combining datasets...")
     combined = xr.merge([obspack_baseline[["latitude", "longitude", "altitude",
                                            "time", "obspack_id", "value", 
@@ -57,20 +71,23 @@ if __name__ == "__main__":
                                 "altitude":"obs_alt", "time":"obs_time", 
                                 "value":"obs_value", "value_unc":"obs_value_unc"})
 
-    # make dimensions site and time
+    # dont want 23:55-23:59 from previous year
+    combined = combined.where(combined["obs_time"] >= pd.to_datetime(f"{PERTURB_START.year}-01-01"), drop=True)
+
+    # make dimensions site and time as in process_geos_output.py
     print("Sorting out dims...")
     combined = combined.assign_coords(obs_time=combined["obs_time"])
     combined = combined.swap_dims({"obs":"obs_time"})
 
-    # drops air sites and non-baseline points
+    # drops air sites and non-baseline points as in process_geos_output.py
     combined = combined.where(combined["baseline"], drop=True)
 
-    # rescale AGAGE to NOAA
+    # rescale AGAGE to NOAA as in process_geos_output.py
     agage_mask = combined["network"] == "AGAGEsurf"
     agage_over_noaa_ratio = pd.read_csv(OBSPACK_DIR / "agage_noaa_scaling/agage_over_noaa_ratio.csv", index_col=0).iloc[0].values[0]
     combined["obs_value"][agage_mask] = combined["obs_value"][agage_mask] / agage_over_noaa_ratio
 
-    # combine NOAA sites and AGAGE sites where we have AGAGE data
+    # combine NOAA sites and AGAGE sites where we have AGAGE data as in process_geos_output.py
     for site in AGAGE_SITES:
         combined["site"].loc[combined["site"] == f"{site.lower()}AGAGEsurf"] = f"{site.lower()}NOAGsurf"
         combined["site"].loc[combined["site"] == f"{site.lower()}NOAAsurf"] = f"{site.lower()}NOAGsurf"
@@ -93,7 +110,7 @@ if __name__ == "__main__":
         # work out square of model std for each obs
         model_std_sq = onesite[["CH4_sum", "obs_floor"]].groupby("obs_floor").std() ** 2
         # recombine with correct obs_time
-        onesite_ninth["model_std"] = (("obs_time"), model_std_sq["CH4_sum"])
+        onesite_ninth["model_std"] = (("obs_time"), model_std_sq["CH4_sum"].values)
         # calculate error in the mean
         onesite_resampled = onesite_ninth["model_std"].resample(obs_time="M").sum()
         no_obs = onesite_ninth["model_std"].resample(obs_time="M").count()
@@ -105,8 +122,6 @@ if __name__ == "__main__":
     print("Recombining sites...")
     site_combined = xr.concat(resampled_sites, dim="site")
     site_combined["site"] = (("site"), unique_sites)
-
-
 
 
     # save combined file
