77820340924ed02cc10346bafa3cb0bf3086bb36
Angharad Stell <as16992@bristol.ac.uk>
2022-02-04 12:58:08 +0000

    add in starting form prior, removed unused code

--- n2o_inv/inversion/mcmc-samples.R
+++ n2o_inv/inversion/mcmc-samples.R
@@ -28,23 +28,14 @@ if (is.null(model_case$process_model$H)) {
   gc(verbose = FALSE)
 }
 
-# check running with tensorflow if desired
-tensorflow_switch <- Sys.getenv("WOMBAT_TENSORFLOW") == "1" && is.null(model_case$measurement_model[["rho"]])
-log_info(sprintf("Using tensorflow: %d", tensorflow_switch))
-
-# check there are no sites with one observation
-no_obs_each_site <- sapply(1:nlevels(model_case$measurement_model$attenuation_factor),
-                           function(i) sum(model_case$measurement_model$attenuation_factor == levels(model_case$measurement_model$attenuation_factor)[i]))
-#log_info(print("Number of observations at each site:"))
-#log_info(print(no_obs_each_site))
-
 log_info("Running MCMC")
 output <- inversion_mcmc(
   11000,
+  start = list("alpha" = rep(0, ncol(model_case$process_model$H))),
   model_case$measurement_model,
   model_case$process_model,
   show_progress = FALSE,
-  use_tensorflow = tensorflow_switch
+  use_tensorflow = 0
 )
 
 log_info("Saving")
