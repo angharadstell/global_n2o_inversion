116c30ab2108411837094cad9e127972503f0ddb
Angharad Stell <as16992@bristol.ac.uk>
2022-02-15 09:12:45 +0000

    working new moving_window_inversion

--- n2o_inv/moving_window/moving_window_inversion.sh
+++ n2o_inv/moving_window/moving_window_inversion.sh
@@ -6,14 +6,17 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=11-00:00:00
 #SBATCH --mem=5G
+#SBATCH --array=0-3
+
+CASE_ARRAY=(IS-RHO0-VARYA-VARYW-NOBIAS-model-err-arbitrary IS-RHO0-FIXEDA-VARYW-NOBIAS-model-err-arbitrary IS-RHO0-VARYA-VARYW-NOBIAS-model-err-n2o_std IS-RHO0-FIXEDA-VARYW-NOBIAS-model-err-n2o_std)
+CASE=${CASE_ARRAY[$SLURM_ARRAY_TASK_ID]}
+echo $CASE
 
 source ~/.bashrc
 conda activate wombat
 
 # read in variables
 source ../spinup/bash_var.sh
-
-./make_intermediates.sh
 
 nwindow=${moving_window[n_window]}
 analytical=FALSE
@@ -23,17 +26,21 @@ do
     echo "starting window $window"
     window02d=`printf %02d $window`
 
-    sed -i -e "s/window02d=.*/window02d=$window02d/" make_models.sh
-    ./make_models.sh
+    sed -e "s/%window02d%/$window02d/" -e "s/%CASE%/$CASE/" make_models.sh > make_models_$CASE.sh
+    chmod +x make_models_$CASE.sh
+    ./make_models_$CASE.sh
+    rm make_models_$CASE.sh
 
     echo "Doing inversion..."
     if [ $analytical = TRUE ]
     then
-        Rscript analytical_inversion.R --window $window
+        Rscript analytical_inversion.R --window $window --case $CASE
 
     else
-        sed -i -e "s/window02d=.*/window02d=$window02d/" make_real_mcmc_samples_vary_submit.sh
-        ./make_real_mcmc_samples_vary_submit.sh
+        sed -e "s/%window02d%/$window02d/" -e "s/%CASE%/${CASE}_window$window02d/" make_real_mcmc_samples_submit.sh > make_real_mcmc_samples_submit_$CASE.sh
+        chmod +x make_real_mcmc_samples_submit_$CASE.sh
+        ./make_real_mcmc_samples_submit_$CASE.sh
+        rm make_real_mcmc_samples_submit_$CASE.sh
     fi
 
     # bring in spinup fluxes
@@ -42,9 +49,9 @@ do
         echo "changing ic..."
         if [ $analytical = TRUE ]
         then
-            Rscript change_control_mf.R --window $window --method "analytical"
+            Rscript change_control_mf.R --window $window --case $CASE --method "analytical"
         else
-            Rscript change_control_mf.R --window $window --method "mcmc"
+            Rscript change_control_mf.R --window $window --case $CASE --method "mcmc"
         fi
     fi
 
